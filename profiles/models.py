31c4aaa12efaec3bb9ff784db3d86b5b8ad7b831
Florian Hass <f91hama@gmail.com>
2024-09-23 10:15:34 +0000

    feat: Add profile creation on user registration
    - Implemented a signal that creates a corresponding Profile instance
      whenever a new User is created.
    - Connected the `create_profile` function to the `post_save` signal
      of the User model to ensure automatic profile creation.

--- profiles/models.py
+++ profiles/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django import Pillow
+
 
 # Create your models here.
 
-class Porfile(models.Model):
-    owner = models.OneToOneField(User, on_delete=Models.CASCADE)
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
-    content = models.CharField(blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default = '../pp5-walkthrough/botfh2hlxd6cudukcqj6.jpg'
     )
@@ -20,3 +21,11 @@ class Porfile(models.Model):
     
     def __str__(self):
         return f"{self.owner}s' profile"
+
+
+def create_profile(profile, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
