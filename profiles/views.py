9a67aa6dfc449324522ee8cc3d92fd94f5af4ad6
Florian Hass <f91hama@gmail.com>
2024-09-23 14:47:35 +0000

    Connect URLs for ProfileList view in the app

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
-from rest_framework. response import Response
+from rest_framework.response import Response
 from .models import Profile
 
 class ProfileList(APIView):
-    def get(self, request)
-    profile = Profile.objects.all()
-    return Response(profiles)
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
