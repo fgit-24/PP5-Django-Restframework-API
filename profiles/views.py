8237caff8fad7acc3e4987669c01cd706e8ce6f5
Florian Hass <f91hama@gmail.com>
2024-09-23 18:38:45 +0000

    Freeze updates in requirements.txt

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 class ProfileList(APIView):
     def get(self, request):
