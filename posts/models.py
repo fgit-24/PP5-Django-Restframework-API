3c7b0665eaf75c5fec1f458b1a4ee92a2e26e1a2
Florian Hass <f91hama@gmail.com>
2024-09-24 15:08:47 +0000

    feat(serializer): add validation for image size and dimensions
    - Added `validate_field_name` method to enforce the following validations:
      - Ensure the image size is less than 2MB.
      - Restrict image width to a maximum of 4096px.
      - Restrict image height to a maximum of 4096px.
    
    This change ensures that uploaded images meet the required size and dimension constraints.

--- posts/models.py
+++ posts/models.py
@@ -24,6 +24,9 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
         ordering = ['-created_at']
