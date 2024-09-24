d0b85f564bb4ee3b387f7270bb6290cb8aa147d9
Florian Hass <f91hama@gmail.com>
2024-09-24 20:48:43 +0000

    feat: Add PostDetail view with get, put, and delete methods
    - Added `get_object` method to handle fetching a post and checking object permissions.
    - Implemented `get` method to retrieve post details with serialized data.
    - Added `put` method to update an existing post and handle validation.
    - Added `delete` method to allow deletion of a post.
    - Updated `urls.py` to include the path for `PostDetail` view using `<int:pk>` parameter for posts.

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts', views.PostList_asView, )
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view())
 ]
