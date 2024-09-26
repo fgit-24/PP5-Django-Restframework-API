005c8f7ec5551d787d302ccadb182dd79dfbd5b3
Florian Hass <f91hama@gmail.com>
2024-09-26 16:16:56 +0000

    feat: Add FollowerList view for listing and creating follower relationships
    - Implemented FollowerList view to list all follower relationships between users.
    - Enabled authenticated users to follow other users by creating new follower instances.
    - Added perform_create method to automatically associate the logged-in user as the owner when creating a follower.
    - Applied IsAuthenticatedOrReadOnly permission to restrict follower creation to authenticated users while allowing read access for all.

--- followers/models.py
+++ followers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .models import Follower
 from django.contrib.auth.models import User
 
 
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
