5f630bfdea5f63e48c253135c2e20274fa59a330
Florian Hass <f91hama@gmail.com>
2024-09-27 14:12:32 +0000

    Add filter for profiles followed by users the owner is following

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -27,7 +28,6 @@ class PostList(generics.ListCreateAPIView):
         'likes__owner__profile',
         'owner__profile',
     ]
-
     search_fields = [
         'owner__username',
         'title',
