cb63d8a780328ce24e34728e83d329c530a8af6f
Florian Hass <f91hama@gmail.com>
2024-10-21 19:46:45 +0000

    Change message on my social_api view

--- social_api/views.py
+++ social_api/views.py
@@ -1,14 +1,8 @@
-from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view()
 def root_route(request):
-    if settings.DEBUG:
-        return Response({
-            'message': 'My drf API, for my photo sharing website, social.',
-        })
-    else:
-        return Response({
-            'message': 'This is a production environment.',
-        })
+    return Response({
+        'message': 'My drf API, for my photo sharing website, social.'
+    })
