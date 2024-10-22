e3967699f926554d7f534561217774a19d30c91f
Florian Hass <f91hama@gmail.com>
2024-10-22 09:05:20 +0000

    Implement logout view and urls

--- social_api/views.py
+++ social_api/views.py
@@ -1,9 +1,40 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
+from django.conf import settings
 
 @api_view()
 def root_route(request):
     return Response({
-        'message': 'My drf API, for my photo sharing website, social.'
+        'message': 'My DRF API, for my photo sharing website, social.'
     })
+
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    
+    # Access the JWT_AUTH settings from the REST_AUTH dictionary
+    jwt_auth_cookie = settings.REST_AUTH['JWT_AUTH_COOKIE']
+    jwt_auth_refresh_cookie = settings.REST_AUTH['JWT_AUTH_REFRESH_COOKIE']
+    jwt_auth_samesite = settings.REST_AUTH['JWT_AUTH_SAMESITE']
+    jwt_auth_secure = settings.REST_AUTH['JWT_AUTH_SECURE']
+    
+    response.set_cookie(
+        key=jwt_auth_cookie,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
+    )
+    response.set_cookie(
+        key=jwt_auth_refresh_cookie,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
+    )
+    
+    return response
